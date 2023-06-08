9717014680f1e9c697b5acd5e34637fcaefc3ade
sergeytompson <gfhfahfp@gmail.com>
2023-06-08 03:01:39 +0300

    fix: corrected docker, clarified README.md, optimized querys

--- restaurant/bin/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-python ../restaurant/manage.py makemigrations
-python ../restaurant/manage.py migrate
-python ../restaurant/manage.py loaddata initial_data.json
--- restaurant/menu/views.py
+++ restaurant/menu/views.py
@@ -1,12 +1,14 @@
-from django.db.models import Prefetch
+from django.db.models import Count, Q
 from rest_framework import generics
 
-from .models import Food, FoodCategory
+from .models import FoodCategory
 from .serializers import FoodListSerializer
 
 
 class FoodCategoryList(generics.ListAPIView):
     serializer_class = FoodListSerializer
-    queryset = FoodCategory.objects.filter(
-        id__in=Food.objects.values_list("category")
-    ).prefetch_related(Prefetch("food", queryset=Food.objects.all()))
+    queryset = FoodCategory.objects.annotate(
+        cnt_food=Count("food", filter=Q(food__is_publish=True))
+    ).filter(
+        cnt_food__gt=0
+    ).prefetch_related("food__additional")
